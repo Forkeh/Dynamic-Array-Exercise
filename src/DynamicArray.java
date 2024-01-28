c4f9a3b3597c4365a329933b5eeffa1746cec0ee
Forkeh <forkeh@gmail.com>
2024-01-28 18:02:58 +0100

    initial commit

---
+++ src/DynamicArray.java
@@ -0,0 +1,32 @@
+public class DynamicArray {
+    private Person[] array;
+
+    public void add(Person person) {
+
+    }
+
+    public int get(int index) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void remove() {
+
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public void set(int index, Person person) {
+
+    }
+
+    public void clear() {
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person harry = new Person("Harry Potter");
+
+        System.out.println(harry);
+
+    }
+}
