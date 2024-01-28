5f7a582904fcb948037c35e432b45f3459c6408f
Forkeh <forkeh@gmail.com>
2024-01-28 19:50:59 +0100

    finished first half of array methods

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,19 +1,41 @@
 public class DynamicArray {
-    private Person[] array;
+    private int ArrMaxSize = 10;
+    private Person[] array = new Person[ArrMaxSize];
+    private int elementsInArray = 0;
 
     public void add(Person person) {
-
+        array[elementsInArray] = person;
+        System.out.println("Added person: " + array[elementsInArray]);
+        elementsInArray++;
     }
 
-    public int get(int index) {
+    public void get(int index) {
+        try {
+        if(index < 0 || array.length <= index) {
+            throw new ArrayIndexOutOfBoundsException("Index out of bounds!");
+        }
+        System.out.println("Person at index " + index + " is: " + array[index]);
 
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
-    public int size() {
-
+    public void size() {
+        System.out.println("Size of the array: " + elementsInArray);
     }
 
     public void remove() {
+        try {
+        if(elementsInArray <= 0) throw new ArrayIndexOutOfBoundsException("Nothing to remove, the array is empty!");
+
+        System.out.println("removed: " + array[elementsInArray-1] + " from index: " + (elementsInArray-1));
+        array[elementsInArray] = null;
+        elementsInArray--;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         Person harry = new Person("Harry Potter");
+        Person snape = new Person("Snape");
+        Person hermione = new Person("Hermione");
 
-        System.out.println(harry);
+        DynamicArray dynArr = new DynamicArray();
+
+        dynArr.size();
+        dynArr.add(harry);
+        dynArr.size();
+        dynArr.add(snape);
+        dynArr.size();
+        dynArr.get(11);
+        dynArr.remove();
+        dynArr.remove();
+        dynArr.remove();
+        dynArr.size();
+        dynArr.add(hermione);
 
     }
 }
