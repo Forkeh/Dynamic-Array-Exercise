7e12fab1ebbcac2efb7b32793967ea3e3c1a7dc9
Forkeh <forkeh@gmail.com>
2024-01-31 12:13:08 +0100

    more stuff

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,20 +1,26 @@
+import java.util.Arrays;
+
 public class DynamicArray {
-    private int ArrMaxSize = 10;
-    private Person[] array = new Person[ArrMaxSize];
+    private final int INITIAL_SIZE = 10;
+    private int size = INITIAL_SIZE;
+    private Person[] array = new Person[INITIAL_SIZE];
     private int elementsInArray = 0;
 
     public void add(Person person) {
         array[elementsInArray] = person;
-        System.out.println("Added person: " + array[elementsInArray]);
         elementsInArray++;
+
+        if (elementsInArray >= INITIAL_SIZE && elementsInArray % INITIAL_SIZE == 0) grow();
+
+        System.out.println(Arrays.toString(array));
     }
 
     public void get(int index) {
         try {
-        if(index < 0 || array.length <= index) {
-            throw new ArrayIndexOutOfBoundsException("Index out of bounds!");
-        }
-        System.out.println("Person at index " + index + " is: " + array[index]);
+            if (index < 0 || array.length <= index) {
+                throw new ArrayIndexOutOfBoundsException("Index out of bounds!");
+            }
+            System.out.println("Person at index " + index + " is: " + array[index]);
 
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Error: " + e.getMessage());
@@ -22,16 +28,21 @@ public class DynamicArray {
     }
 
     public void size() {
-        System.out.println("Size of the array: " + elementsInArray);
+        System.out.println("Elements in the array: " + elementsInArray);
     }
 
     public void remove() {
         try {
-        if(elementsInArray <= 0) throw new ArrayIndexOutOfBoundsException("Nothing to remove, the array is empty!");
+            if (elementsInArray <= 0)
+                throw new ArrayIndexOutOfBoundsException("Nothing to remove, the array is empty!");
 
-        System.out.println("removed: " + array[elementsInArray-1] + " from index: " + (elementsInArray-1));
-        array[elementsInArray] = null;
-        elementsInArray--;
+//            System.out.println("removed: " + array[elementsInArray - 1] + " from index: " + (elementsInArray - 1));
+            array[elementsInArray - 1] = null;
+            elementsInArray--;
+
+            if (elementsInArray > INITIAL_SIZE && elementsInArray % INITIAL_SIZE != 0) shrink();
+
+            System.out.println("remove: " + Arrays.toString(array));
 
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
@@ -40,15 +51,41 @@ public class DynamicArray {
     }
 
     public void remove(int index) {
-
+        for (int i = index; i < elementsInArray; i++) {
+            array[i] = array[i + 1];
+            array[i + 1] = null;
+        }
+        System.out.println("After removal: " + Arrays.toString(array));
+        this.elementsInArray--;
     }
 
     public void set(int index, Person person) {
-
+        array[index] = person;
+        System.out.println("After set: " + Arrays.toString(array));
     }
 
     public void clear() {
-
+        array = new Person[INITIAL_SIZE];
+        System.out.println("After clear: " + Arrays.toString(array));
     }
 
+    private void grow() {
+        Person[] newArray = new Person[elementsInArray + INITIAL_SIZE];
+
+        for (int i = 0; i < elementsInArray; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+        System.out.println("GROW");
+    }
+
+    private void shrink() {
+        Person[] newArray = new Person[elementsInArray - INITIAL_SIZE];
+
+        for (int i = 0; i < elementsInArray; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+        System.out.println("SHRINK!");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,37 @@ public class Main {
         Person harry = new Person("Harry Potter");
         Person snape = new Person("Snape");
         Person hermione = new Person("Hermione");
+        Person martin = new Person("Martin");
 
         DynamicArray dynArr = new DynamicArray();
 
         dynArr.size();
         dynArr.add(harry);
-        dynArr.size();
         dynArr.add(snape);
-        dynArr.size();
-        dynArr.get(11);
-        dynArr.remove();
-        dynArr.remove();
-        dynArr.remove();
-        dynArr.size();
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(martin);
         dynArr.add(hermione);
-
+        dynArr.add(hermione);
+        dynArr.add(harry);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(snape);
+        dynArr.add(martin);
+        dynArr.add(hermione);
+        dynArr.add(hermione);
+        dynArr.size();
+        dynArr.remove();
+        dynArr.remove();
+        dynArr.remove();
+        dynArr.size();
     }
 }
